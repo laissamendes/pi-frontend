3dd4c235b8be6f0121a895112027a213792ac7f5
Laíssa Mendes <laissa.mendes3006@gmail.com>
2024-08-22 09:24:23 -0300

    inicialização da página pessoa

--- src/views/PessoaView.vue
+++ src/views/PessoaView.vue
@@ -1,9 +1,21 @@
 <script setup></script>
 
 <template>
-    oi
+    <label for="nome">Nome Completo:</label>
+    <input type="text" name="nome" id="input-infos">
+
+    <label for="cpf">CPF:</label>
+    <input type="text" name="cpf" id="input-infos">
 </template>
 
 <style scoped>
-
+#input-infos{
+    color:#FEFAE0;
+    background-color: #606c3869;
+    border-style: none;
+    border-radius: 20px;
+}
+label{
+    color: #6A2C0F;
+}
 </style>
