9896204853b6c8bc94ae468c25dec9b9d76e7fe0
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-08-30 17:01:40 -0300

    alteração na página de pessoa na parte dos inputs

--- src/views/PessoaView.vue
+++ src/views/PessoaView.vue
@@ -1,22 +1,54 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <label for="nome">Nome Completo:</label>
-    <input type="text" name="nome" id="input-infos">
+  <div class="inputs">
+    <div class="input-nome">
+      <label for="nome">Nome Completo:</label>
+      <input type="text" name="nome" id="input-infos" />
+    </div>
 
-    <label for="cpf">CPF:</label>
-    <input type="text" name="cpf" id="input-infos">
+    <div class="input-cpf">
+      <label for="cpf">CPF:</label>
+      <input type="text" name="cpf" id="input-infos" />
+    </div>
+
+    <input type="submit" name="submit" id="input-submit" />
+  </div>
+
+  <div v-if="">
+  </div>
 </template>
 
 <style scoped>
-#input-infos{
-    color:#FEFAE0;
-    background-color: #606c3869;
-    border-style: none;
-    border-radius: 20px;
+#input-infos {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  width: 30rem;
+  height: 2rem;
+  color: #fefae0;
+  background-color: #606c3869;
+  border-style: none;
+  border-radius: 20px;
 }
-label{
-    color: #6A2C0F;
+#input-submit {
+  color: #fefae0;
+  background-color: #606c38;
+  border-style: none;
+  border-radius: 20px;
+}
+label {
+  color: #6a2c0f;
+}
+.inputs {
+  display: flex;
+  justify-content: space-around;
+  justify-items: baseline;
+}
+
+#input-submit {
+  width: 10rem;
+  height: 2rem;
+  font-size: 1rem;
 }
 </style>
