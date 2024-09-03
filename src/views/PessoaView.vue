94586c92ee6319516688c4cc6f97b439cb6f2aca
AnaFidencio <fidenciosilvaanaclara@gmail.com>
2024-09-03 11:12:13 -0300

    criação dos inputs e estilização do css

--- src/views/PessoaView.vue
+++ src/views/PessoaView.vue
@@ -1,54 +1,8 @@
 <script setup></script>
 
 <template>
-  <div class="inputs">
-    <div class="input-nome">
-      <label for="nome">Nome Completo:</label>
-      <input type="text" name="nome" id="input-infos" />
-    </div>
-
-    <div class="input-cpf">
-      <label for="cpf">CPF:</label>
-      <input type="text" name="cpf" id="input-infos" />
-    </div>
-
-    <input type="submit" name="submit" id="input-submit" />
-  </div>
-
-  <div v-if="">
-  </div>
+ oi
 </template>
 
 <style scoped>
-#input-infos {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1rem;
-  width: 30rem;
-  height: 2rem;
-  color: #fefae0;
-  background-color: #606c3869;
-  border-style: none;
-  border-radius: 20px;
-}
-#input-submit {
-  color: #fefae0;
-  background-color: #606c38;
-  border-style: none;
-  border-radius: 20px;
-}
-label {
-  color: #6a2c0f;
-}
-.inputs {
-  display: flex;
-  justify-content: space-around;
-  justify-items: baseline;
-}
-
-#input-submit {
-  width: 10rem;
-  height: 2rem;
-  font-size: 1rem;
-}
 </style>
--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -1,9 +1,53 @@
 <script setup></script>
 
 <template>
-    oi
+    <form class="form">
+    <h1 class="titulo">Registros</h1>
+    <div class="inputs">
+    <div class="input-nome">
+      <input type="text" name="nome" id="input-infos"  placeholder="NOME COMPLETO"/>
+    </div>
+    <div>
+    <p class="data">Data de Nascimento</p>
+  <input id="date" type="date" value="2000-01-01" />
+</div>
+    <div class="input-cpf">
+      <input type="text" name="cpf" id="cpf"  v-cpf
+  maxlength="14"
+  placeholder="CPF" />
+    </div>
+
+    <div class="input-nome">
+      <input type="text" name="nome" id="input-infos"  placeholder="PAI"/>
+    </div>
+    <div class="input-nome">
+      <input type="text" name="nome" id="input-infos"  placeholder="MÃE" />
+    </div>
+    <input type="submit" name="submit" id="input-submit" />
+  </div>
+</form>
 </template>
 
 <style scoped>
-
+form{
+    background-color: #606C38;
+    opacity: 0.7; /* 50% de opacidade */
+    margin-left: 180px;
+    margin-right: 180px;
+    height: 500px;
+}
+.titulo{
+    text-align: center;
+    color: #FEFAE0;
+    font-weight: bold;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+.data{
+    color: #FEFAE0;
+    font-weight: bold;
+}
+.inputs{
+    text-align:right;
+    margin-right: 145px;
+}
 </style>
