92d8ce33ee48df5c42969a0ad6f7820ff92008e9
Laíssa Mendes <laissa.mendes3006@gmail.com>
2024-09-12 08:41:35 -0300

    hover adcionado nos btns da página inicial style do header alterado cor de fundo geral alterada

--- src/components/templates/medium/MediumHeader.vue
+++ src/components/templates/medium/MediumHeader.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { onBeforeRouteUpdate } from 'vue-router'
 import LogoTitle from '@/components/templates/LogoTitle.vue'
-import HeaderTitle from '@/components/templates/HeaderTitle.vue'
+import HeaderTitle from '../HeaderTitle.vue'
 
 const showMenu = ref(false)
 
@@ -12,75 +12,89 @@ onBeforeRouteUpdate(() => {
 </script>
 
 <template>
-  <div class="logo-title">
-    <logo-title />
-    <header-title />
-  </div>
-
-  <div id="headerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
-    <router-link to="/Animal"> <i class="mdi mdi-dog" /> Animal </router-link>
-    <router-link to="/Moradia"> <i class="mdi mdi-home" /> Moradia </router-link>
-    <router-link to="/Pessoa"> <i class="mdi mdi-account" /> Pessoa </router-link>
-    <router-link to="/Registro"> <i class="mdi mdi-file-document" /> Registro </router-link>
-  </div>
-  <div class="icons">
-    <div class="hamburger" @click="showMenu = !showMenu">
-      <i class="mdi mdi-menu"></i>
+  <header class="header-container">
+    <div class="logo-title">
+      <logo-title />
+      <HeaderTitle />
     </div>
-  </div>
-</template>
+
+    <div class="icons">
+      <div class="hamburger" @click="showMenu = !showMenu">
+        <i class="mdi mdi-menu"></i>
+      </div>
+    </div>
+
+    <div id="headerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
+      <router-link to="/Animal"> <i class="mdi mdi-dog" /> Animal </router-link>
+      <router-link to="/Moradia"> <i class="mdi mdi-home" /> Moradia </router-link>
+      <router-link to="/Pessoa"> <i class="mdi mdi-account" /> Pessoa </router-link>
+      <router-link to="/Registro"> <i class="mdi mdi-file-document" /> Registro </router-link>
+    </div>
+  </header>
+  </template>
+
 <style scoped>
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 20px;
+  background-color: #fefae0;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative; 
+  height: 125px;
+}
+
+.logo-title {
+  display: flex;
+  align-items: center;
+  color: #6a2c0f;
+}
+
+.icons {
+  display: flex;
+  align-items: center;
+}
+
 #headerMenu {
-  position: fixed;
-  top: 0%;
+  position: absolute; 
+  top: 100%; 
   right: 0;
   z-index: 1;
-  width: 20%;
+  width: 200px;
   background-color: #fefae0;
   color: #606c38;
-  display: block;
   padding: 1rem;
   border-radius: 10px;
   box-shadow: 15px 5px 10px #919c704f;
+  display: none;
 }
 
 #headerMenu a {
   display: flex;
-  width: 100%;
+  align-items: center;
   text-decoration: none;
   background-color: #fefae0;
   font-size: 1.2rem;
   transition: color 0.3s;
   color: #606c38;
+  padding: 0.5rem 0;
 }
 
-.icons {
-  display: flex;
-  width: 100%;
-  margin: 10px;
+#headerMenu a:hover {
+  color: #a3b18a;
 }
 
-.icons a,
 .icons .hamburger {
   display: flex;
-  flex-direction: column;
+  align-items: center;
   text-decoration: none;
   color: #6a2c0f;
   font-size: 2rem;
-  transition: color 0.3s;
-  position: fixed;
-  top: 0%;
-  right: 0;
-  z-index: 1;
-  margin-right: 10px;
+  cursor: pointer;
 }
 
 .hamburger:hover {
-  cursor: pointer;
-}
-.logo-title {
-  display: flex;
-  justify-items: baseline;
-  color: #6a2c0f;
+  color: #a3b18a;
 }
 </style>
